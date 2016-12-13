23dc1caf01d8a7f6de2bf5ba66b330584faa35f4
gcaglion@yahoo.com <gcaglion@yahoo.com>
2016-12-13 14:38:21 +0100

    fixed Rubik.cmd ; added CompileRubik.cmd for manual operations

---
+++ CompileRubik.cmd
@@ -0,0 +1,1 @@
+javac -cp C:\OpenCV\3.1.0\build\java\opencv-310.jar -d .\bin src\my3d\*.java src\rubik\*.java
--- Rubik.cmd
+++ Rubik.cmd
@@ -1,2 +1,3 @@
-set CLASSPATH=C:/Users/gcaglion/Documents/Dev/git/Rubik/bin;C:/OpenCV/3.1.0/build/java/opencv-310.jar
+set CLASSPATH=./bin;C:/OpenCV/3.1.0/build/java/opencv-310.jar
+
 java rubik.RGui
